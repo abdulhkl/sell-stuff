e71e66ebaacf5f52ae346e88cf29b1bfcf534172
Abdul Haseeb <abdulhk79@gmail.com>
2024-05-05 02:35:30 +0530

    logo amends

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,101 +1,100 @@
-import { useState } from 'react'
-import toast from "react-hot-toast"
-import { Link, useNavigate } from 'react-router-dom'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
-import OAuth from '../components/OAuth'
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import OAuth from '../components/OAuth';
 
-import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
-import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 function SignIn() {
-    const [showPassword, setShowPassword] = useState(false)
-    const [formData, setFormData] = useState({
-        email: '',
-        password: ''
-    })
-    const { email, password } = formData
-    const navigate = useNavigate()
-    const onChange = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.id]: e.target.value
-        }))
-    }
+	const [showPassword, setShowPassword] = useState(false);
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+	const { email, password } = formData;
+	const navigate = useNavigate();
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.id]: e.target.value,
+		}));
+	};
 
-    const onSubmit = async (e) => {
-        e.preventDefault()
+	const onSubmit = async (e) => {
+		e.preventDefault();
 
-        try {
-            const auth = getAuth()
+		try {
+			const auth = getAuth();
 
-            const userCredential = await signInWithEmailAndPassword(
-                auth,
-                email,
-                password
-            )
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
 
-            if (userCredential.user) {
-                navigate('/profile')
-            }
-        } catch (error) {
-            toast.error("Bad user credential")
-        }
-    }
-    return (
-        <>
-            <div className='pageContainer'>
-                <header>
-                    <p className='pageHeader'>Welcome Back!</p>
-                </header>
-                <div className='formWrapper'>
-                    <form onSubmit={onSubmit}>
-                        <input
-                            type='email'
-                            className='emailInput'
-                            placeholder='Email'
-                            id='email'
-                            value={email}
-                            onChange={onChange}
-                            required
-                        />
+			if (userCredential.user) {
+				navigate('/profile');
+			}
+		} catch (error) {
+			toast.error('Bad user credential');
+		}
+	};
+	return (
+		<>
+			<div className='pageContainer'>
+				<header>
+					<p className='pageHeader'>Welcome Back!</p>
+				</header>
+				<div className='formWrapper'>
+					<form onSubmit={onSubmit}>
+						<input
+							type='email'
+							className='emailInput'
+							placeholder='Email'
+							id='email'
+							value={email}
+							onChange={onChange}
+							required
+						/>
 
-                        <div className='passwordInputDiv'>
-                            <input
-                                type={showPassword ? 'text' : 'password'}
-                                className='passwordInput'
-                                placeholder='Password'
-                                id='password'
-                                value={password}
-                                onChange={onChange}
-                                required
-                            />
+						<div className='passwordInputDiv'>
+							<input
+								type={showPassword ? 'text' : 'password'}
+								className='passwordInput'
+								placeholder='Password'
+								id='password'
+								value={password}
+								onChange={onChange}
+								required
+							/>
 
-                            <img
-                                src={visibilityIcon}
-                                alt='show password'
-                                className='showPassword'
-                                onClick={() => setShowPassword((prevState) => !prevState)}
-                            />
-                        </div>
-                        <Link to='/forgot-password' className='forgotPasswordLink'>
-                            Forgot Password
-                        </Link>
-                        <div className='signInBar'>
-                            <button className='signInButton'>
-                                <span className='signInText'>Sign In</span>
-                                <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
-                            </button>
-                        </div>
-
-                    </form>
-                </div>
-                <OAuth />
-                <Link to='/sign-up' className='registerLink'>
-                    Sign Up Instead
-                </Link>
-            </div>
-        </>
-    )
+							<img
+								src={visibilityIcon}
+								alt='show password'
+								className='showPassword'
+								onClick={() => setShowPassword((prevState) => !prevState)}
+							/>
+						</div>
+						<Link to='/forgot-password' className='forgotPasswordLink'>
+							Forgot Password
+						</Link>
+						<div className='signInBar'>
+							<button className='signInButton'>
+								<span className='signInText'>Sign In</span>
+								<ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+							</button>
+						</div>
+					</form>
+				</div>
+				<OAuth />
+				<Link to='/sign-up' className='registerLink'>
+					Sign Up Instead
+				</Link>
+			</div>
+		</>
+	);
 }
 
-export default SignIn
+export default SignIn;
